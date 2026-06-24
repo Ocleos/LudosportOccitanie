b30a5977ff75a48b7c22634bd111fc960e889f91
Ocleos <ocleos@gmail.com>
2026-06-24 10:18:16 +0200

    feat(app): update start 26-27

--- src/components/events/events.component.tsx
+++ src/components/events/events.component.tsx
@@ -9,13 +9,12 @@ const Events = () => {
         Vous souhaitez nous rencontrer et discuter avec nous, n'hésitez pas à venir aux prochains événements :
       </p>
 
-      <EventCard description='Forum des associations - Nîmes' startDate='06/09/2025' />
+      <EventCard description='VitalSport Décathlon - Nîmes' endDate='30/08/2026' startDate='29/08/2026' />
+      <EventCard description='Antigone des Associations - Montpellier' startDate='06/09/2026' />
 
-      <EventCard description='Antigone des Associations - Montpellier' startDate='07/09/2025' />
+      <EventCard description='Reprise des cours' endDate='11/09/2026' startDate='07/09/2026' />
 
-      <EventCard description='Reprise des cours' endDate='12/09/2025' startDate='08/09/2025' />
-
-      <EventCard description='VitalSport Décathlon - Nîmes' endDate='14/09/2025' startDate='13/09/2025' />
+      <EventCard description='VitalSport Décathlon - Montpellier' endDate='13/09/2026' startDate='12/09/2026' />
     </div>
   );
 };
